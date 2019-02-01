83e03f35ca31141e60cc02a77d0dbaab1a07e661
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-02-01 15:27:58 -0200

    create test feature to user_sign_in_steps

--- features/_cucumber_works.feature
+++
@@ -1,6 +0,0 @@
-Feature: Welcome
-  I want to see the home page of my app
- 
-  @javascript
-  Scenario: View welcome page
-    Given I am on the home page
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,28 +5,20 @@ require 'database_cleaner/cucumber'
 World(FactoryBot::Syntax::Methods)
 
 Capybara.app_host = "http://#{ENV['TEST_APP_HOST']}:#{ENV['TEST_PORT']}"
-Capybara.javascript_driver = :selenium
-Capybara.run_server = false
-
-args = ['--no-default-browser-check', '--start-maximized']
-caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => args})
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
-      desired_capabilities: caps
-  )
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
- 
+
+Capybara.javascript_driver = :chrome
+
 ActionController::Base.allow_rescue = false
- 
+
 begin
   DatabaseCleaner.strategy = :truncation
 rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+  raise 'You need to add database_cleaner to your Gemfile (in the :test group)'
 end
- 
+
 Around do |scenario, block|
   DatabaseCleaner.cleaning(&block)
 end
