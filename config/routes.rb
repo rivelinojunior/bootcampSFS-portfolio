6cdeac561a31dd1fb0fd225a83075ab2570412e0
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-02-02 20:37:23 -0200

    create controller to languages block

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :educations, only: %i[index create destroy]
     resources :skills, only: %i[index create destroy]
     resources :hobbies, only: %i[index create destroy]
+    resources :languages, only: %i[index create destroy]
   end
   resources :tags, only: :index
 end
