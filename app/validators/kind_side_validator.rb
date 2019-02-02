0ae911cb1da415a6e17ccdefccb81b0cf7b9d4ed
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-02-01 23:51:01 -0200

    create custom validation to kind side

---
+++ app/validators/kind_side_validator.rb
@@ -0,0 +1,24 @@
+class KindSideValidator < ActiveModel::Validator
+  def validate(record)
+    @record = record
+    validate_side_kind if @record.side.present?
+  end
+
+  private
+
+  def left_kind?
+    %w[profile about social contact additional_information]
+      .include? @record.kind
+  end
+
+  def right_kind?
+    %w[education experience hobby skill language additional_information]
+      .include? @record.kind
+  end
+
+  def validate_side_kind
+    return if send("#{@record.side}_kind?")
+
+    @record.errors.add(:base, "can't be at #{@record.side} side")
+  end
+end
