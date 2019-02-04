f26815c031c09c6ea1c74f90a1e15cb5cb82343a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-02-04 13:07:09 -0200

    move images to public folder and fix your paths

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,7 +7,7 @@ class Profile < ApplicationRecord
   validates :photo, file: { max_size_in_bytes: 3000, mime_types: %i[jpg png] }
 
   def photo_url
-    return "/assets/avatars/#{portfolio.temp_avatar}.png" unless photo.attached?
+    return "/avatars/#{portfolio.temp_avatar}.png" unless photo.attached?
 
     Rails.application.routes.url_helpers.rails_blob_path(photo, only_path: true)
   end
